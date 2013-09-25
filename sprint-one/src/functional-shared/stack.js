deedda24d2c0e22580b9e88aa053a101ee19bcfc
Hack Reactor Students <github@hackreactor.com>
2013-09-25 15:21:53 -0700

    complete all tests for functional shared / stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,40 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
 
-var stackMethods = {};
+var makeStack = function() {
+
+  var instance = {
+    storage: {},
+    len: 0
+  };
+  extend(instance, stackMethods);
+  return instance;
+};
+
+var stackMethods = {
+  push: function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  },
+
+  pop: function(){
+    if(this.len > 0){
+      var popped = this.storage[this.len-1];
+      delete this.storage[this.len];
+      this.len--;
+      return popped;
+    }
+    return 0;
+  },
+
+  size: function(){
+    return this.len;
+  }
+
+};
+
+
 
