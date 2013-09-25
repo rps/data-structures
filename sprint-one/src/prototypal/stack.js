af8ab177a81f1829f89d7060122965fa35118150
Hack Reactor Students <github@hackreactor.com>
2013-09-25 15:54:05 -0700

    completed all tests for prototypal / stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,27 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.len = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.len] = value;
+    this.len++;
+  },
 
+  pop: function(){
+    if(this.len > 0){
+      var popped = this.storage[this.len-1];
+      delete this.storage[this.len];
+      this.len--;
+      return popped;
+    }
+    return 0;
+  },
+
+  size: function(){
+    return this.len;
+  }
+};
