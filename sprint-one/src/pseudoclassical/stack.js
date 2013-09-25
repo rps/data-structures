59e9ec75368d22b3a7005b4c7c9530efb29a592f
Hack Reactor Students <github@hackreactor.com>
2013-09-25 16:06:17 -0700

    completed all tests for pseudoclassical / stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,23 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.len = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.storage[this.len] = value;
+  this.len++;
+};
+
+Stack.prototype.pop = function(){
+  if(this.len > 0){
+    var popped = this.storage[this.len-1];
+    delete this.storage[this.len];
+    this.len--;
+    return popped;
+  }
+  return 0;
+};
+
+Stack.prototype.size = function(){
+  return this.len;
+};
