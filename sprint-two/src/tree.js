e184095c225cec405a58370f01394f1d6114a0cf
Hack Reactor Students <github@hackreactor.com>
2013-09-26 11:29:40 -0700

    built tree.js with children,addchild and contains methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,43 @@
-var makeTree = function(){
-  var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
-
+var makeTree = function(value){
+  var newTree = {
+    children : [],
+    // parent : null, // for extra credit later
+    value : value || null 
+  };
+  extend(newTree, treeMethods);
   return newTree;
 };
 
-var treeMethods = {};
+var treeMethods = {
+  addChild : function(value){
+    this.children.push(makeTree(value)); 
+  },
 
-treeMethods.addChild = function(){
+  contains : function(value){
+    if (this.value === value){
+      return true;
+    } else {
+      for (var i = 0; i < this.children.length; i++){
+        if (this.children[i].contains(value)){
+          return true;
+        }
+      }
+      return false;
+    } 
+  }
+};
+  //return isThisIt? || _.any(children, isThisit?)
+
+  // removeFromParent: function(){ // for extra credit later
+var extend = function(tree, treeMethods) {
+  for (var keys in treeMethods){
+    tree[keys] = treeMethods[keys];
+  }
 };
 
-treeMethods.contains = function(){
-};
+// var newtree = makeTree(5);
+// newtree.addChild(10);
+// newtree.addChild('x');
+// debugger;
+// newtree.contains('x');
+
