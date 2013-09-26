6bc04809167ebe4fd6135581cbf7143bd7bd1512
Hack Reactor Students <github@hackreactor.com>
2013-09-25 22:40:06 -0700

    doubly linked linkedlist.js / addtohead() and removetail()

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,22 +1,31 @@
 // Note: don't use an array to do this.
-var makeLinkedList = function(){
+var makeLinkedList = function(h, t){
   var list = {
-    head : makeNode(12),
-    tail : makeNode(26) 
+    head : makeNode(h),
+    tail : makeNode(t) 
   };
   list.head.next = list.tail;
+  list.tail.previous = list.head;
 
   list.addToTail = function(val){
     var last = makeNode(val);
+    last.previous = list.tail;
     list.tail.next = last;
     list.tail = last;
   };
+
+  list.addToHead = function(headNum){
+    var first = makeNode(headNum, this.head);
+    this.head = first;
+    this.head.next.previous = this.head;
+  }
 
   list.removeHead = function(){
     var result = list.head.value;
     var buffer = list.head.next;
     delete list.head;
     list.head = buffer;
+    list.head.previous = null;
     return result;
   };
 
@@ -36,7 +45,11 @@ var makeLinkedList = function(){
 var makeNode = function(value){
   var node = {};
   node.value = value;
-  node.next = null;
+  node.next = arguments[1] || null;
+  node.previous = arguments[2] || null;
 
   return node;
 };
+
+var x = makeLinkedList(5,10)
+console.log(x)
