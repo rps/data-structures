4d2092f9fc0ef5662261cb50feed4064fb1c3dc6
Hack Reactor Students <github@hackreactor.com>
2013-09-26 14:28:42 -0700

    partial solution for set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,39 @@
 var makeSet = function(){
-  var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  var set = Object.create(setPrototype); 
+  set._storage = {};
+  extend(set, setPrototype);
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(input){ 
+  debugger;
+  this._storage[input] = (input in this._storage) ? this._storage[input] : null;
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(input){ 
+  return (input in this._storage) ? true : false;
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(input){
+  delete this._storage[input];
 };
+
+var extend = function(to, from){
+  for (var keys in from){
+    to[keys] = from[keys];
+  }
+};
+
+
+var myObj = makeSet();
+myObj.add("hello");
+myObj.add("goodbye");
+console.log(myObj);
+//myObj.contains("hello");
+myObj.remove("farewell");
+console.log(myObj);
+myObj.remove("goodbye");
+console.log(myObj);
+//myObj.contains("goodbye");
