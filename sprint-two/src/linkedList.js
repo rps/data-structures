5dd51c58b4a37bc796d46179de4276a46c9bc0bd
Hack Reactor Students <github@hackreactor.com>
2013-09-25 21:31:18 -0700

    addtotail and removehead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,27 @@
 // Note: don't use an array to do this.
 var makeLinkedList = function(){
-  var list = {};
-  list.head = null;
-  list.tail = null;
+  var list = {
+    head : makeNode(12), //labels
+    tail : makeNode(26)  //labels
+  };
+  list.head.next = list.tail;
 
-  list.addToTail = function(){
+  list.addToTail = function(val){
+    var last = makeNode(val);
+    list.tail.next = last;
+    list.tail = last;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    var buffer = list.head.next;
+    delete list.head;
+    list.head = buffer;
+    return result;
   };
 
   list.contains = function(){
+    
   };
 
   return list;
@@ -23,3 +34,5 @@ var makeNode = function(value){
 
   return node;
 };
+var m = makeLinkedList();
+console.log(m)
