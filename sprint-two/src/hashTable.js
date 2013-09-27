aab751a15dab3e223a01d6929494b370d2471bda
Hack Reactor Students <github@hackreactor.com>
2013-09-26 21:21:23 -0700

    completed hashTable.js & included test file

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,31 +1,120 @@
 var HashTable = function(){
   this._limit = 8;
-
-  // Use a limited array to store inserted elements.
-  // It'll keep you from using too much space. Usage:
-  //
-  //   limitedArray.set(3, 'hi');
-  //   limitedArray.get(3); // alerts 'hi'
-  //
-  // There's also a '.each' method that you might find
-  // handy once you're working on resizing
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  this._storage.set(i, v);
+  var i = getIndexBelowMaxForKey(k, this._limit); // (str, max)
+  var inserted = {};
+  inserted[k] = v;
+  if (this._storage.get(i) !== undefined){ //has a key already
+    var found = this._storage.get(i);
+    found[k] = v;
+    this._storage.set(i, found);
+  } else {
+    this._storage.set(i, inserted);
+  }
 };
+
+  // later increase size of hashtable
+
+  // if(this._storage.get(i) !== undefined) {
+  //   var oldVal = this._storage.get(k);
+  //   var obj = {};
+  //   obj[oldVal] = k;
+  //   obj[v] = true;
+  //   this._storage.set(i,obj);
+  // } else {
+  //   this._storage.set(i, v);
+  // }
+  // var num = 0;
+  // this._storage.each(function(value){
+  //   if(value !== undefined){
+  //     num++;
+  //   }
+  // });
+  // if(num / this._limit > 0.75){
+  //   //this._limit *= 2;
+  //   debugger;
+  //   var this._newStorage = makeLimitedArray(this._limit*2);
+  //   this._storage.each(function(v, k){
+  //     var result = this._storage.retrieve(k); 
+  //     this._newStorage.set(getIndexBelowMaxForKey(k, this._limit),result); //index, value
+  //   });
+  // }
+// when getting, provide K to _.storage GET function. THEN if Object,
+// use K to determine what to return
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var found = this._storage.get(i);
+  return found[k];
 };
 
-HashTable.prototype.remove = function(){
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var found = this._storage.get(i);
+  delete found[k];
+  this._storage.set(i,found);
 };
+
+// use each to count non-undefined values in storage vs storage size (limit?)
+// if non-undefined / limit > .75, then each from end of storage array to fill 
+// with undefined
+
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you
+
+/*
+ ********** NOTE: **********
+ * Do not edit this code unless you see a bug!
+ */
+
+
+// This class represents an array with limited functionality and a maximum size.
+// It will ensure that you don't accidentally try to use up too much space.
+//
+// Usage:
+//   limitedArray.set(3, 'hi');
+//   limitedArray.get(3); // returns 'hi'
+
+var makeLimitedArray = function(limit){
+  var storage = []; //private storage that only limitedArray has access to
+
+  var limitedArray = {};
+  limitedArray.get = function(index){
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value){
+    checkLimit(index);
+    storage[index] = value;
+  };
+  limitedArray.each = function(callback){
+    for(var i = 0; i < storage.length; i++){
+      callback(storage[i], i, storage);
+    }
+  };
+
+  var checkLimit = function(index){
+    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
+    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+  };
+
+  return limitedArray;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
---
+++ sprint-two/src/hashTest.js
@@ -0,0 +1,86 @@
+var HashTable = function(){
+  this._limit = 8;
+
+  // Use a limited array to store inserted elements.
+  // It'll keep you from using too much space. Usage:
+  //
+  //   limitedArray.set(3, 'hi');
+  //   limitedArray.get(3); // alerts 'hi'
+  //
+  // There's also a '.each' method that you might find
+  // handy once you're working on resizing
+  this._storage = makeLimitedArray(this._limit);
+};
+
+HashTable.prototype.insert = function(k, v){
+  console.log('k: ',k,'v: ',v);
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  console.log('i: ',i);
+  this._storage.set(i, v);
+};
+
+HashTable.prototype.retrieve = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
+};
+
+HashTable.prototype.listStorage = function(){
+  this._storage.listStorage();
+};
+
+/*
+ ********** NOTE: **********
+ * Do not edit this code unless you see a bug!
+ */
+
+
+// This class represents an array with limited functionality and a maximum size.
+// It will ensure that you don't accidentally try to use up too much space.
+//
+// Usage:
+//   limitedArray.set(3, 'hi');
+//   limitedArray.get(3); // returns 'hi'
+
+var makeLimitedArray = function(limit){
+  var storage = [];
+
+  var limitedArray = {};
+  limitedArray.get = function(index){
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value){
+    checkLimit(index);
+    storage[index] = value;
+  };
+  limitedArray.each = function(callback){
+    for(var i = 0; i < storage.length; i++){
+      callback(storage[i], i, storage);
+    }
+  };
+  limitedArray.listStorage = function(){
+      console.log(this.each(function(val){console.log(val)}));
+  };   
+
+
+
+  var checkLimit = function(index){
+    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
+    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+  };
+
+  return limitedArray;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between the
+// numbers 0 and `max`
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
